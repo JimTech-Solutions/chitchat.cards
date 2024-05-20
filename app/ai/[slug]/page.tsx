ed4f667a78a8d06545835b29d3bdab8a22265f69
Jim <mrjim.development@gmail.com>
2024-05-20 23:00:15 +0800

    feature: ai generated questions

---
+++ app/ai/[slug]/page.tsx
@@ -0,0 +1,274 @@
+"use client"
+import React, { useEffect, useRef, useState } from 'react'
+import { CiFilter, CiHeart } from 'react-icons/ci'
+import { HiArrowPathRoundedSquare, HiArrowUturnLeft, HiArrowUturnRight } from 'react-icons/hi2'
+import { PiShuffle } from 'react-icons/pi'
+
+import { createClient } from '@supabase/supabase-js'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import Slider from "react-slick";
+
+import { Categories, Questions, GameData, GameQuestion, GameCategory} from '@/types/games'
+import { GoogleAnalytics } from '@next/third-parties/google'
+import Header from '@/components/Header'
+import Head from 'next/head'
+import { checkUserAccess, getAuthUser } from '@/app/supabase-client'
+import WelcomeForm from '@/components/welcome_form'
+import { UserProvider } from '@/context/UserContext'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+interface Option {
+    id: number,
+    category: string,
+    checked: boolean,
+}
+
+interface SelectedOption {
+  id: string | number; // Ensure this matches the 'id' type in GameCategory
+  category: string;
+  checked: boolean;
+}
+
+const Page: React.FC = () => {
+    const pathname = usePathname();
+    const slug = pathname?.split('/').pop();
+
+
+    const [gameData, setGameData] = useState<any>([])
+    const [questions, setQuestions] = useState<Questions[]>([]);
+    const [categoriesData, setCategoriesData] = useState<Categories[] | any>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const router = useRouter();
+
+    const [hasAccess, setHasAccess] = useState(false);
+    const [loadingAccessCheck, setLoadingAccessCheck] = useState(false);
+
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (dropdownRef.current && event.target instanceof Node && !dropdownRef.current.contains(event.target)) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+
+    const [selectedOptions, setSelectedOptions] = useState<Option[]>([]);
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    const shuffleArray = (array: any[]) => {
+        let arr = [...array];
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap elements
+        }
+        return arr;
+    }
+
+    useEffect(() => {
+        if (!slug) return; // Do nothing if slug is not yet available
+
+        // const checkUser = async () => {
+        //     const user = await getAuthUser();
+
+        //     if (!user) {
+        //         router.push('/signin?redirect='+ pathname);
+        //     }
+        // }
+
+        // checkUser();
+
+        const fetchQuestions = async () => {
+        // let { data: result, error: gameError } = await supabase
+        // .rpc('fetchgamebyslug', {
+        //     p_slug: slug
+        // }).single();
+
+        
+        let { data: generated_questions, error } = await supabase
+        .from('generated_questions')
+        .select('*').eq('game_id', slug).single()
+
+        // console.log(result);
+
+        if (generated_questions) {
+            console.log(generated_questions.questions);
+            setQuestions(shuffleArray(generated_questions.questions));
+        } else {
+            console.log(error);
+        }
+
+        
+
+        setLoading(false);
+        };
+
+        fetchQuestions();
+    }, [slug]);
+
+    useEffect(() => {
+        const initialSelectedOptions: Option[] = categoriesData.map((category: { id: string | number; category: string }) => ({
+            id: category.id,
+            category: category.category,
+            checked: true 
+        }));
+        setSelectedOptions(initialSelectedOptions);
+    }, [categoriesData]);
+
+    const handleShuffleButton = () => {
+        setQuestions(shuffleArray(questions));
+        if (sliderRef.current) {
+            sliderRef.current.slickGoTo(0);
+        }
+    };
+
+    const handleFilterChange = (changedCategory : GameCategory) => {
+
+        setSelectedOptions((prevSelectedOptions) => {
+            const updatedOptions = prevSelectedOptions.map((option) => {
+                if (option.category === changedCategory.category) {
+                    return { ...option, checked: !option.checked }; // Toggle the 'checked' state
+                }
+                return option;
+            });
+
+            return updatedOptions;
+        });
+    };
+
+    const filteredQuestions = questions.filter(question =>
+        selectedOptions.some(option => option.category === question.category && option.checked)
+    );
+
+    // console.log('selected:', selectedOptions);
+
+    const getCategoryColor = (category: string) => {
+        const foundCategory = categoriesData.find((cat : GameCategory) => cat.category == category);
+        return foundCategory ? foundCategory.color : 'white'; 
+    };
+
+    let sliderRef = useRef<Slider | null>(null);
+    const next = () => {
+        sliderRef.current?.slickNext();
+    };
+
+    const previous = () => {
+        sliderRef.current?.slickPrev();
+    };
+
+    const settings = {
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
+  return (
+    <UserProvider> 
+        <Head>
+            <title>ChitChat: Interactive Conversation Games for Better Connections</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+            <meta name="description" content="Boost your relationships with ChitChat! Explore interactive games and intriguing questions designed for couples and friends." />
+
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content="ChitChat: Interactive Conversation Games for Better Connections" />
+            <meta property="og:description" content="Boost your relationships with ChitChat! Explore interactive games and intriguing questions designed for couples and friends." />
+            <meta property="og:image" content="https://chitchat.cards/images/thumbnail.png" />
+            <meta property="og:url" content="https://chitchat.cards" />
+
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content="ChitChat: Interactive Conversation Games for Better Connections" />
+            <meta name="twitter:description" content="Boost your relationships with ChitChat! Explore interactive games and intriguing questions designed for couples and friends." />
+            <meta name="twitter:image" content="https://chitchat.cards/images/thumbnail.png" />
+        </Head>
+        <Header />
+        <section className="flex  flex-col justify-center  relative mt-5">
+
+                {loading && (
+                    <div className="slider-container my-[150px] xl:my-[300px] flex items-center justify-center"> 
+                        <div className="w-12 h-12 rounded-full animate-spin border-y border-solid border-5 border-yellow-500 border-t-transparent"></div>
+                    </div>
+                )}
+                <div className="slider-container my-[100px] xl:my-[250px]">
+                    <Slider ref={sliderRef}
+                        {...settings}>
+                        {questions.map((question, index) => {
+                            const textColor = getCategoryColor(question.category);
+                            console.log(question);
+                            return (
+                            <div key={index} className="text-center p-10 my-auto justify-center items-center">
+                                    <p className={`mb-3`} style={{ color: textColor }}>{question.category.toUpperCase()}</p>
+                                    <p className="text-2xl">{question.question}</p>
+                            </div>
+                            )
+                        })}
+                    </Slider>
+                </div>
+
+        
+            <div className="flex justify-center w-full items-center gap-3 xl:gap-5 mt-20  bottom-20 fixed">
+                <button className="rounded-2xl p-5 bg-[#272727] hover:opacity-50" onClick={previous}>
+                    <HiArrowUturnLeft size="24"/>
+                </button>
+
+                <button className="rounded-2xl p-5 bg-[#272727] hover:opacity-50" onClick={handleShuffleButton}>
+                    <PiShuffle size="24" />
+                </button>
+
+                <div className="relative" ref={dropdownRef}>
+                    <button
+                        className="rounded-2xl p-5 bg-[#272727] hover:opacity-50"
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        <CiFilter size={24} />
+                    </button>
+
+                    {isOpen && (
+                        <div className="absolute z-10 bg-[#272727] border-black rounded-md shadow-lg bottom-full mb-3 p-3 overflow-auto max-h-80 w-[200px] left-[-50px]">
+                        {/* Content of your dropdown */}
+                        <p className="mb-2">Filters</p>
+                        {categoriesData.map((option : GameCategory, index : number) => (
+                            <div key={index} className="flex items-start gap-2">
+                            <input
+                                type="checkbox"
+                                id={option.category}
+                                checked={selectedOptions.some(selectedOption => selectedOption.category === option.category && selectedOption.checked)}
+                                onChange={() => handleFilterChange(option)} // Pass the whole category object
+                                className="form-checkbox min-h-5 min-w-5 text-blue-600"
+                            />
+                            <label className="flex items-center space-x-3cursor-pointer" htmlFor={option.category}>
+                                <span className="flex-1">{option.category} <span className="text-sm">({option.question_count})</span></span>
+                            </label>
+                            </div>
+                        ))}
+                        </div>
+                    )}
+                    </div>
+
+                <button className="rounded-2xl p-5 bg-[#272727] hover:opacity-50" onClick={next}>
+                    <HiArrowUturnRight size="24"/>
+                </button>
+            </div>
+
+            <WelcomeForm />
+            
+            <GoogleAnalytics gaId="G-X05HE2M1XM" />
+        </section>
+    </UserProvider>
+  )
+}
+
+export default Page
--- components/contact_form.tsx
+++ components/contact_form.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { Dispatch, SetStateAction } from 'react';
 
 import Link from 'next/link'
 import { Fragment, useEffect, useRef, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { IoClose } from 'react-icons/io5'
 import { FaWaveSquare } from 'react-icons/fa'
-import {Steps, useSteps } from "react-step-builder";
+import { Steps, useSteps } from "react-step-builder";
 import { UserIcon } from '@heroicons/react/24/outline'
 import { createClientSupabaseClient, getAuthUser } from '@/app/supabase-client'
 import { Resend } from 'resend';
@@ -44,7 +44,11 @@ const schema = z.object({
 });
 type FormFields = z.infer<typeof schema>;
 
-const ContactForm: React.FC = () => {
+interface ContactFormProps {
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const ContactForm: React.FC<ContactFormProps> = ({ setOpen }) => {
 
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
@@ -62,107 +66,118 @@ const ContactForm: React.FC = () => {
     // console.log(formData);
 
     try {
-        const response = await fetch('/api/sendEmail', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ 
-            subject: `${formData.purpose}`, 
-            header: `${formData.subject}`,
-            content: `${formData.content}`,
-            reply_to: `${formData.email}`
-          }),
-        });
+      const response = await fetch('/api/sendEmail', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          subject: `${formData.purpose}`,
+          header: `${formData.subject}`,
+          content: `${formData.content}`,
+          reply_to: `${formData.email}`
+        }),
+      });
 
-        if (!response.ok) throw new Error('Failed to send email');
+      if (!response.ok) throw new Error('Failed to send email');
 
-        const result = await response.json();
-        console.log(result.message);
-        setShowSuccessMessage(true); // Show success message
-        reset(); // Clear the form fields
+      const result = await response.json();
+      console.log(result.message);
+      setShowSuccessMessage(true); // Show success message
+      reset(); // Clear the form fields
 
-      } catch (error) {
-        console.error('Email sending error:', error);
-      }
+    } catch (error) {
+      console.error('Email sending error:', error);
+    }
 
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-      {showSuccessMessage && (
-        <div className="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
-          Your message has been sent successfully!
+    <>
+      <div className="mb-4">
+        <p className="text-2xl text-primary mb-2">Contact Us</p>
+        <p className="text-gray-300 text-md">Let us know you questions, inquiries and concerns. We'll get back to you as soon as we can.</p>
+      </div>
+
+      <button className="absolute top-0 right-0 p-3 m-3 " onClick={() => setOpen(false)}>
+        <IoClose size={24} />
+      </button>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        {showSuccessMessage && (
+          <div className="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+            Your message has been sent successfully!
+          </div>
+        )}
+        <div className="flex gap-2">
+          <div className="w-full">
+            <label htmlFor="first_name" className="block text-sm font-medium">First Name</label>
+            <input
+              type="text"
+              id="first_name"
+              className="block w-full rounded-md border-0 py-2 px-4 ring-1 ring-inset ring-gray-300 bg-transparent placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-300 sm:text-sm sm:leading-6"
+              {...register('first_name')}
+            />
+          </div>
+          <div className="w-full">
+            <label htmlFor="last_name" className="block text-sm font-medium">Last Name</label>
+            <input
+              type="text"
+              id="last_name"
+              className="block w-full rounded-md border-0 py-2 px-4 ring-1 ring-inset ring-gray-300 bg-transparent placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-300 sm:text-sm sm:leading-6"
+              {...register('last_name')}
+            />
+          </div>
         </div>
-      )}
-      <div className="flex gap-2"> 
-        <div className="w-full">
-          <label htmlFor="first_name" className="block text-sm font-medium">First Name</label>
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium">Email</label>
+          <input
+            type="email"
+            id="email"
+            className="block w-full rounded-md border-0 py-2 px-4 ring-1 ring-inset ring-gray-300 bg-transparent placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-300 sm:text-sm sm:leading-6"
+            {...register('email')}
+          />
+          {errors.email && <p className="mt-2 text-sm text-red-600">{errors.email.message}</p>}
+        </div>
+        <div>
+          <label htmlFor="purpose" className="block text-sm font-medium">Purpose</label>
+          <select
+            id="purpose"
+            className="block w-full rounded-md border-0 py-3 px-4 ring-1 ring-inset ring-gray-300 bg-transparent placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-300 sm:text-sm sm:leading-6"
+            {...register('purpose')}
+          >
+            <option className="bg-[#151515]" value="">Select a purpose</option>
+            <option className="bg-[#151515]" value="General Inquiry">General Inquiry</option>
+            <option className="bg-[#151515]" value="Feature Request">Feature Request</option>
+            <option className="bg-[#151515]" value="Technical Support">Technical Support</option>
+            <option className="bg-[#151515]" value="Billing">Billing</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="subject" className="block text-sm font-medium">Subject</label>
           <input
             type="text"
-            id="first_name"
+            id="subject"
             className="block w-full rounded-md border-0 py-2 px-4 ring-1 ring-inset ring-gray-300 bg-transparent placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-300 sm:text-sm sm:leading-6"
-            {...register('first_name')}
+            {...register('subject')}
           />
         </div>
-        <div className="w-full">
-          <label htmlFor="last_name" className="block text-sm font-medium">Last Name</label>
-          <input
-            type="text"
-            id="last_name"
+        <div>
+          <label htmlFor="content" className="block text-sm font-medium">Content</label>
+          <textarea
+            id="content"
             className="block w-full rounded-md border-0 py-2 px-4 ring-1 ring-inset ring-gray-300 bg-transparent placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-300 sm:text-sm sm:leading-6"
-            {...register('last_name')}
+            {...register('content')}
           />
+          {errors.content && <p className="mt-2 text-sm text-red-600">{errors.content.message}</p>}
         </div>
-      </div>
-      <div>
-        <label htmlFor="email" className="block text-sm font-medium">Email</label>
-        <input
-          type="email"
-          id="email"
-          className="block w-full rounded-md border-0 py-2 px-4 ring-1 ring-inset ring-gray-300 bg-transparent placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-300 sm:text-sm sm:leading-6"
-          {...register('email')}
-        />
-        {errors.email && <p className="mt-2 text-sm text-red-600">{errors.email.message}</p>}
-      </div>
-      <div>
-        <label htmlFor="purpose" className="block text-sm font-medium">Purpose</label>
-        <select
-          id="purpose"
-          className="block w-full rounded-md border-0 py-3 px-4 ring-1 ring-inset ring-gray-300 bg-transparent placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-300 sm:text-sm sm:leading-6"
-          {...register('purpose')}
-        >
-          <option className="bg-[#151515]" value="">Select a purpose</option>
-          <option className="bg-[#151515]" value="General Inquiry">General Inquiry</option>
-          <option className="bg-[#151515]" value="Feature Request">Feature Request</option>
-          <option className="bg-[#151515]" value="Technical Support">Technical Support</option>
-          <option className="bg-[#151515]" value="Billing">Billing</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="subject" className="block text-sm font-medium">Subject</label>
-        <input
-          type="text"
-          id="subject"
-          className="block w-full rounded-md border-0 py-2 px-4 ring-1 ring-inset ring-gray-300 bg-transparent placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-300 sm:text-sm sm:leading-6"
-          {...register('subject')}
-        />
-      </div>
-      <div>
-        <label htmlFor="content" className="block text-sm font-medium">Content</label>
-        <textarea
-          id="content"
-          className="block w-full rounded-md border-0 py-2 px-4 ring-1 ring-inset ring-gray-300 bg-transparent placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-300 sm:text-sm sm:leading-6"
-          {...register('content')}
-        />
-        {errors.content && <p className="mt-2 text-sm text-red-600">{errors.content.message}</p>}
-      </div>
-      <div>
-        <button type="submit" className="text-center text-[#151515] bg-primary rounded-lg px-6 py-3 font-semibold text-sm shadow-md hover:opacity-80 flex gap-1 items-centers justify-center my-4" disabled={isSubmitting} > 
-              {isSubmitting ? 'Loading...' : 'Submit'}
+        <div>
+          <button type="submit" className="text-center text-[#151515] bg-primary rounded-lg px-6 py-3 font-semibold text-sm shadow-md hover:opacity-80 flex gap-1 items-centers justify-center my-4" disabled={isSubmitting} >
+            {isSubmitting ? 'Loading...' : 'Submit'}
           </button>
-      </div>
-    </form>
+        </div>
+      </form>
+    </>
   );
 };
 
